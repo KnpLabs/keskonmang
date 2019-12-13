e9fbcdd13609c58e2ed258c2aec4b077c938088d
Joris Langlois <joris.langlois@knplabs.com>
2019-12-13 14:59:33 +0100

    only add bearer flag if a token exists

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,12 +1,20 @@
+import { pipe, propOr } from 'ramda'
+
 // getToken :: GoogleApi -> String
-const getToken = googleApi => 
-  googleApi()
+const getToken = pipe(
+  googleApi => googleApi()
     .auth2
     .getAuthInstance()
     .currentUser
     .get()
     .getAuthResponse(true)
-    .id_token
+  ,
+  propOr(null, 'id_token'),
+)
+
+const defaultHeaders = {
+  'Content-Type': 'application/json'
+}
 
 // createFetchApi :: (Fetcher, GoogleApi) -> String -> Object -> Promise
 export const createFetchApi = (fetcher, googleApi) => (url, options) =>
@@ -14,10 +22,12 @@ export const createFetchApi = (fetcher, googleApi) => (url, options) =>
     `${process.env.REACT_APP_API_BASE_URL}${url}`,
     {
       ...options,
-      headers: {
-        'Authorization': `Bearer ${getToken(googleApi)}`,
-        'Content-Type': 'application/json'
-      },
+      headers: getToken(googleApi) !== null
+        ? {
+          'Authorization': `Bearer ${getToken(googleApi)}`,
+          ...defaultHeaders,
+        }
+        : defaultHeaders
     }
   ).then(response => response.status < 400
     ? response.json()
