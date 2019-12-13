709e37dd28c81109d6edc2a50f0d23bd2b461a69
Emma <emmanuelle.collet@knplabs.com>
2019-12-13 14:42:35 +0100

    Fix review

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../../Style/Images/Logo-keskonmang.svg'
 import '../../Style/Logo.css'
 
-// Logo :: Props -> Reac.Component
+// Logo :: Props -> React.Component
 export default () => 
   <figure className="logo"> 
     <img src={logo} alt="logo" />  
