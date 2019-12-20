a8649d4203953f4a52a673ab3ad86b31eda45bdd
Joris Langlois <joris.langlois@knplabs.com>
2019-12-20 16:14:14 +0100

    Fixed mobile design for home page

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -3,7 +3,10 @@ import logo from '../../Style/Images/Logo-keskonmang.svg'
 import './Logo.css'
 
 // Logo :: Props -> React.Component
-export default ({restaurantShown}) => 
-  <figure className={`logo ${restaurantShown ? 'small' : ''}`}> 
-    <img src={logo} alt="logo" />  
+export default ({restaurantShown}) =>
+  <figure
+    data-is="brand-logo"
+    className={`${restaurantShown ? 'is-small' : ''}`}
+  >
+    <img src={logo} alt="logo" />
   </figure>
