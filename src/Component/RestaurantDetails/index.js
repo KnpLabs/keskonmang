f1ba89151d94d2ee72b101e8c1714df10e101f50
Emma <emmanuelle.collet@knplabs.com>
2021-08-27 10:35:57 +0200

    Allow to display a restaurant details from url

--- src/Component/RestaurantDetails/index.js
+++ src/Component/RestaurantDetails/index.js
@@ -1,28 +1,33 @@
 import RestaurantDetails from './RestaurantDetails'
 import { connect } from 'react-redux'
-import { clear } from './../../Redux/State/RestaurantDetails'
+import { clear, getRestaurantDetails } from './../../Redux/State/RestaurantDetails'
 import { getRestaurant } from './../../Redux/State/RestaurantWheel'
 import { addHistory } from './../../Redux/State/History'
 import { compose } from 'ramda'
-import { componentWillUnmount } from 'react-functional-lifecycle'
+import { componentDidMount, componentWillUnmount } from 'react-functional-lifecycle'
 
 // mapStateToProps :: State -> Props
 const mapStateToProps = state => ({
   restaurant: state.RestaurantDetails.restaurant,
   loading: state.RestaurantDetails.loading,
-  isLogged: state.Session.user !== null
+  isLogged: state.Session.user !== null,
+  searchAddress: state.RestaurantWheel.address,
 })
 
 // mapDispatchToProps :: (Action * -> State) -> Props
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, { restaurantId, isAlreadyLoaded }) => ({
+  getRestaurantDetails: !isAlreadyLoaded 
+    ? compose(dispatch, getRestaurantDetails, () => restaurantId) 
+    : () => {},
   getRestaurant: compose(dispatch, getRestaurant),
   addHistory: compose(dispatch, addHistory),
   clear: compose(dispatch, clear),
 })
 
-const willUnmount = ({ clear }) => clear()
-
-const lifecycles = componentWillUnmount(willUnmount)(RestaurantDetails)
+const lifecycles = compose(
+  componentDidMount(({ getRestaurantDetails }) => getRestaurantDetails()),
+  componentWillUnmount(({ clear }) => clear()),
+)(RestaurantDetails)
 
 // RestaurantDetails :: Props -> React.Component
 export default connect(
